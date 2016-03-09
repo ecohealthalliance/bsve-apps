d395054a8d298798829e78fe7c557d1cde91b0d1
Brock Arnold <arnold@ecohealthalliance.org>
2016-03-09 16:01:48 -0500

    Add flirt and tater BSVE apps, plus update script for updating apps.eha.io

--- apps.eha.io/update.sh
+++ apps.eha.io/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Now syncing with S3"
-aws s3 sync . s3://apps.eha.io --acl public-read
+aws s3 sync . s3://apps.eha.io --acl public-read --exclude "node_modules/*"
 
 echo "Done"
