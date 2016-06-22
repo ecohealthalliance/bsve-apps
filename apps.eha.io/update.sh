37e7d5b5f8a8bbeaffd17986522970282e9da0c9
jgoley <jgoley@gmail.com>
2016-06-22 09:40:26 -0400

    Change update script to sync dist folder

--- apps.eha.io/update.sh
+++ apps.eha.io/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Now syncing with S3"
-aws s3 sync . s3://apps.eha.io --acl public-read --exclude "node_modules/*"
+aws s3 sync ./dist s3://apps.eha.io --acl public-read
 
 echo "Done"
