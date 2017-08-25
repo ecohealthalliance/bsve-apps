2cbb7d2dbea769ae42bf1f42207a071772827599
Abe Miessler <miessler@ecohealthalliance.org>
2017-08-25 08:04:16 -0700

    filtering working but needs fine tuning to get events to fire as soon as filters change

--- EPIs/WebContent/app.js
+++ EPIs/WebContent/app.js
@@ -9,29 +9,12 @@ function initApp()
   authTicket = BSVE.api.authTicket();
   user = BSVE.api.userData();
   fetchData();
-}
 
-function fetchData()
-{
-  var filter = "?$filter=flightyear+ge+2018"
-  // start by seeing if we can simply list the data sources
-  $.ajax({
-    type: "GET",
-    // url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_Airport_Data/data",
-    url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_FlightCount_Data/data" + filter,
-    beforeSend: function(xhr)
-    {
-      xhr.setRequestHeader("harbinger-auth-ticket", authTicket);
-    },
-    success: function(data)
-    {
-      console.log("success data", data)
-      if ( data.result.length )
-      {
-        $('#table').bootstrapTable({
+  $(function(){
+    $('#table').bootstrapTable({
             columns: [{
                 field: 'source',
-                title: 'Source'
+                title: 'Origin'
             }, {
                 field: 'destination',
                 title: 'Destination'
@@ -44,10 +27,92 @@ function fetchData()
             }, {
                 field: 'passengercount',
                 title: 'Passenger Count'
-            } ],
-            data: data.result
+            } ]
+          })
+    getAirports()
+    $(".typeahead").change(function(){
+      console.log("Change!")
+      fetchData()
+    })
+  })
+}
+
+function setupTypeAhead(airportCodes)
+{
+  $("#origin").typeahead({source: airportCodes})
+  $("#destination").typeahead({source: airportCodes})
+}
+
+function getAirports()
+{
+  $.ajax({
+    type: "GET",
+    url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_Airport_Data/data",
+    beforeSend: function(xhr)
+    {
+      xhr.setRequestHeader("harbinger-auth-ticket", authTicket);
+    },
+    success: function(data)
+    {
+      setupTypeAhead($.map(data.result, function(a) { return a["code"]; }))
+    }
+  });
+}
+
+function buildFilter()
+{
+  var origin = $("#origin").val()
+  var dest = $("#destination").val()
+  var year = $("#year :selected").text()
+  var filter = []
+  if(origin){
+    filter.push("source+eq+'"+origin + "'")
+  }
+  if(dest){
+    filter.push("destination+eq+'"+dest + "'")
+  }
+  filter.push("flightyear+eq+" + year)
+  return "$filter=" + filter.join("+and+")
+}
+
+function fetchData()
+{
+  var filter = buildFilter()
+  console.log("filter","https://api.bsvecosystem.net/data/v2/sources/FLIRT_FlightCount_Data/data?" + filter)
+  $.ajax({
+    type: "GET",
+    // url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_Airport_Data/data",
+    url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_FlightCount_Data/data?" + filter,
+    beforeSend: function(xhr)
+    {
+      xhr.setRequestHeader("harbinger-auth-ticket", authTicket);
+    },
+    success: function(data)
+    {
+      console.log("success data", data)
+      if ( data.result.length )
+      {
+        // $('#table').bootstrapTable({
+        //     columns: [{
+        //         field: 'source',
+        //         title: 'Origin'
+        //     }, {
+        //         field: 'destination',
+        //         title: 'Destination'
+        //     }, {
+        //         field: 'flightyear',
+        //         title: 'Year'
+        //     }, {
+        //         field: 'flightcount',
+        //         title: 'Flight Count'
+        //     }, {
+        //         field: 'passengercount',
+        //         title: 'Passenger Count'
+        //     } ],
+        //     data: data.result
             
-        });
+        // });
+        $('#table').bootstrapTable('load', data.result)
       }
       else
       {
