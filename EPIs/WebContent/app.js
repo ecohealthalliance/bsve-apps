76a25e4d8ed88dfe74668dc29869c7fe7e9c0287
Abe Miessler <miessler@ecohealthalliance.org>
2017-08-29 14:17:56 -0700

    adding logic for HotSpots/FLIRT panes

--- EPIs/WebContent/app.js
+++ EPIs/WebContent/app.js
@@ -1,27 +1,44 @@
 BSVE.init(initApp);
 
+// var user = null,
+//   authTicket = null,
+//   dataSource = null
+
 var user = null,
-  authTicket = null,
+  authTicket = "apikey=AK9dc4549a-2f2f-4f96-9a0b-a614fded12cb;timestamp=1503411781906;nonce=915077438;signature=d11e2435e0d230b6c450f5883e5363b3b42c7a04",
   dataSource = null
 $(function(){
+  $("nav li.flirt").click(function(){
+    $("nav li").toggleClass("active")
+    $("#hotpots").hide()
+    $("#flirt").show()
+  });
+
+  $("nav li.hotspots").click(function(){
+    $("nav li").toggleClass("active")
+    $("#hotpots").show()
+    $("#flirt").hide()
+  });
+
   $('#table').bootstrapTable({
-          columns: [{
-              field: 'source',
-              title: 'Origin'
-          }, {
-              field: 'destination',
-              title: 'Destination'
-          }, {
-              field: 'flightyear',
-              title: 'Year'
-          }, {
-              field: 'flightcount',
-              title: 'Flight Count'
-          }, {
-              field: 'passengercount',
-              title: 'Passenger Count'
-          } ]
-        })
+    columns: [{
+        field: 'source',
+        title: 'Origin'
+    }, {
+        field: 'destination',
+        title: 'Destination'
+    }, {
+        field: 'flightyear',
+        title: 'Year'
+    }, {
+        field: 'flightcount',
+        title: 'Flight Count'
+    }, {
+        field: 'passengercount',
+        title: 'Passenger Count'
+    } ]
+  })
+  fetchData()
 })
 
 function initApp()
@@ -56,7 +73,7 @@ function getAirports()
     url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_Airport_Data/data",
     beforeSend: function(xhr)
     {
-      xhr.setRequestHeader("harbinger-auth-ticket", authTicket);
+      xhr.setRequestHeader("harbinger-authentication", authTicket);
     },
     success: function(data)
     {
@@ -85,39 +102,27 @@ function fetchData()
 {
   var filter = buildFilter()
   console.log("filter","https://api.bsvecosystem.net/data/v2/sources/FLIRT_FlightCount_Data/data?" + filter)
+  console.log("auth header", authTicket)
   $.ajax({
     type: "GET",
     // url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_Airport_Data/data",
     url: "https://api.bsvecosystem.net/data/v2/sources/FLIRT_FlightCount_Data/data?" + filter,
     beforeSend: function(xhr)
     {
-      xhr.setRequestHeader("harbinger-auth-ticket", authTicket);
+      xhr.setRequestHeader("harbinger-authentication", authTicket);
+      console.log("xhr", xhr)
+    },
+    error: function(data, status, error){
+      console.log("ERROR!", data, status, error)
+    },
+    complete: function(xhr, data){
+      console.log("complete", xhr, data)
     },
     success: function(data)
     {
       console.log("success data", data)
       if ( data.result.length )
       {
-        // $('#table').bootstrapTable({
-        //     columns: [{
-        //         field: 'source',
-        //         title: 'Origin'
-        //     }, {
-        //         field: 'destination',
-        //         title: 'Destination'
-        //     }, {
-        //         field: 'flightyear',
-        //         title: 'Year'
-        //     }, {
-        //         field: 'flightcount',
-        //         title: 'Flight Count'
-        //     }, {
-        //         field: 'passengercount',
-        //         title: 'Passenger Count'
-        //     } ],
-        //     data: data.result
-            
-        // });
         $('#table').bootstrapTable('load', data.result)
       }
       else
